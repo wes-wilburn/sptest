654f3d9075861e1a9494f2a21b1bd55023a623a6
Weston B. Wilburn <wes.b.wilburn@gmail.com>
2013-02-08 23:51:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sptest::Application.config.secret_token = '49f0c9240fd21fdcdaf1320f5bdbbe881ff209f3393709f4ace7faaf9b8ee0c3c77576a5fc0cdfa4ec20b433aaf68a4b9b302024363644c0204d7ff2fd5a6a9a'
